1b22eee9fb1ede681cfd454ce0907d8753d044d2
Peter Pan <1932287243@qq.com>
2024-12-09 22:22:53 +0800

    2024-12-9:complete source map inst

--- include/sdb/disasm.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DISASM_H__
-#define __DISASM_H__
-
-#include "common.h"
-extern "C" {
-  extern void init_disasm(const char *triple);
-  extern void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-}
-#endif
--- include/ui/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
-#include "mainwindow.h"
-class MainWindow;
-extern MainWindow *w;
-#endif // !__MAIN_H__
